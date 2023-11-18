2f0ac4763249fecd3ccce4d85f6e427c31348d18
Khangphan <102360362+KhangPhan27@users.noreply.github.com>
2023-11-18 21:37:51 +0700

    Update test.py

--- test.py
+++ test.py
@@ -13,5 +13,5 @@ api = client.login(os.environ.get("APPSTATE"), options={
 	"user_agent": "Mozilla/5.0 (Linux; Android 9; SM-G973U Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36"
 })
 
-# api.send_message("hello from fcapy", "4228720243921825")
+# api.send_message("hello from fcapy", "4228720243921825").
 api.listen_mqtt()

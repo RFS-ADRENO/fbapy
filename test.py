905ecfa7bcf10c1f75d222bc6d9188491c7f6454
RFS-ADRENO <dungto76@gmail.com>
2023-11-28 21:28:20 +0700

    test: receive and process message from callback

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-from fcapy.fcapy import *
+from fcapy import *
 
 dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
@@ -13,12 +13,32 @@ api = client.login(os.environ.get("APPSTATE"), options={
 	"user_agent": "Mozilla/5.0 (Linux; Android 9; SM-G973U Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36"
 })
 
-def callback(event):
-	if event is not None:
-		print(f"[{event['type']}] {event['thread_id']} - {event['sender_id']}: {event['body'] or 'Empty message'}")
+PREFIX = "."
 
-		if "attachments" in event and len(event["attachments"]) > 0:
-			for attachment in event["attachments"]:
-				print(f"Attachment: {attachment['type']} - {attachment['preview_url']}")
+def callback(event, api: API):
+  try:
+    if event is not None:
+      if event["type"] == "message" or event["type"] == "message_reply":
+        body = event["body"] # guaranteed to exist
+
+        
+        if body.startswith(PREFIX):
+          if body == PREFIX + "ping":
+            api.send_message("pong", event["thread_id"], event["message_id"])
+          elif body == PREFIX + "hi":
+            api.send_message("hello", event["thread_id"], event["message_id"])
+          else:
+            api.send_message("Unknown command", event["thread_id"], event["message_id"])
+
+        if "attachments" in event and len(event["attachments"]) > 0:
+          for attachment in event["attachments"]:
+            if "preview_url" not in attachment or attachment["type"] == "video":
+              print(f"Attachment: {attachment['type']} - {attachment['url']}")
+            else:
+              print(f"Attachment: {attachment['type']} - {attachment['preview_url']}")
+
+
+  except Exception as e:
+    print(event)
 
 api.listen_mqtt(callback)

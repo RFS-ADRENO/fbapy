e401c3fa6608ea27d82b7f030f1a8a4f29989cd1
KhangPhan27 <duykhang4341@gmail.com>
2023-12-05 00:18:35 +0700

    feat: add change nickname, change emoji, mark read api

--- fcapy/_apis/__init__.py
+++ fcapy/_apis/__init__.py
@@ -1,14 +1,7 @@
 from ._send_message import send_message
 from ._listen_mqtt import listen_mqtt
 from ._resolve_photo_url import resolve_photo_url
-<<<<<<< HEAD
-<<<<<<< HEAD
 from ._change_emoji import change_emoji
 from ._unsend_message import unsend_message
 from ._change_nickname import change_nickname
 from ._read_status import read_status
-
-=======
->>>>>>> e79c252c4a87265d62ff3aca850badeea5d386d2
-=======
->>>>>>> e79c252c4a87265d62ff3aca850badeea5d386d2

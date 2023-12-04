92ce9034b789e9b703b0a73d07127edcbf40b93f
KhangPhan27 <duykhang4341@gmail.com>
2023-12-05 00:26:10 +0700

    feat: add change nickname, change emoji, mark read api

--- fcapy/_apis/_change_emoji.py
+++ fcapy/_apis/_change_emoji.py
@@ -4,5 +4,5 @@ def change_emoji(default_funcs: DefaultFuncs, ctx: dict):
 			form={}
 			form["emoji_choice"] = emoji
 			form["thread_or_other_fbid"] = thread_id
-			print(default_funcs.post_with_defaults("https://www.facebook.com/messaging/save_thread_emoji/?source=thread_settings&__pc=EXP1%3Amessengerdotcom_pkg",form,ctx).text)
+			default_funcs.post_with_defaults("https://www.facebook.com/messaging/save_thread_emoji/?source=thread_settings&__pc=EXP1%3Amessengerdotcom_pkg",form,ctx).text
 		return change

913eff0c228b30a8d5e0c9f57e3cce49b12c5dd1
hnoga-n <hoanghuynguyen711@gmail.com>
2024-01-06 09:54:45 +0700

    refactor(api): format code of change_bio api
    Signed-off-by: RFS-ADRENO <dungto76@gmail.com>

--- fcapy/_apis/_change_bio.py
+++ fcapy/_apis/_change_bio.py
@@ -10,19 +10,19 @@ def change_bio(default_funcs: DefaultFuncs, ctx: dict):
             "fb_api_req_friendly_name": "ProfileCometSetBioMutation",
             "doc_id": "6996613973732391",
             "variables": json.dumps(
-                {"input": {
-                    "attribution_id_v2": "ProfileCometTimelineListViewRoot.react,comet.profile.timeline.list,unexpected,1703941286245,183952,190055527696468,,;ProfileCometTimelineListViewRoot.react,comet.profile.timeline.list,unexpected,1703940792344,513946,190055527696468,,;ProfileCometTimelineListViewRoot.react,comet.profile.timeline.list,via_cold_start,1703940775960,698443,190055527696468,,",
-                    "bio": bio_content,
-                    "publish_bio_feed_story": public_bio,
-                    "actor_id": ctx["user_id"],
-                    "client_mutation_id": "3"
-                },
+                {
+                    "input": {
+                        "attribution_id_v2": "ProfileCometTimelineListViewRoot.react,comet.profile.timeline.list,unexpected,1703941286245,183952,190055527696468,,;ProfileCometTimelineListViewRoot.react,comet.profile.timeline.list,unexpected,1703940792344,513946,190055527696468,,;ProfileCometTimelineListViewRoot.react,comet.profile.timeline.list,via_cold_start,1703940775960,698443,190055527696468,,",
+                        "bio": bio_content,
+                        "publish_bio_feed_story": public_bio,
+                        "actor_id": ctx["user_id"],
+                        "client_mutation_id": "3",
+                    },
                     "hasProfileTileViewID": True,
                     "scale": 1.5,
-                    "useDefaultActor": False
+                    "useDefaultActor": False,
                 }
-            )
+            ),
         }
-        default_funcs.post_with_defaults(
-            "https://www.facebook.com/api/graphql/", form, ctx).text
+        default_funcs.post_with_defaults("https://www.facebook.com/api/graphql/", form, ctx)
     return change
